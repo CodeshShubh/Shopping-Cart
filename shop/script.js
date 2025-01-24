e66fc2998c8da248d9835262d24b89ebe34a62b3
CodeshShubh <codeshshubh@gmail.com>
2025-01-25 04:35:42 +0530

    rendering products

--- shop/script.js
+++ shop/script.js
@@ -1,10 +1,186 @@
-const produtc = {
-  id: 1,
-  title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-  price: 109.95,
-  description:
-    "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-  category: "men's clothing",
-  image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-  rating: { rate: 3.9, count: 120 },
-};
+
+
+let currUser = localStorage.getItem('currUser');
+
+if(currUser){
+  let colors =['red', 'black', 'blue', 'green'];
+  let sizes = ['XS', 'SM', 'MD', 'LG', 'XL'];
+
+  const products = fetch('https://fakestoreapi.com/products').then((response)=>{
+   return  response.json()
+  }).then((data)=>{
+    if(localStorage.getItem('newProducts')){
+       let products = JSON.parse(localStorage.getItem('newProducts'))
+       console.log(products);
+       renderManProducts(products);
+       renderWomensProducts(products);
+       renderJeweleryProducts(products);
+       renderElectronicsProducts(products)
+    }else{
+
+      // adding elememts existing json object using map
+
+      let newProducts = data.map((items)=>{
+        items.colors = colors.slice(Math.floor(Math.random()*4))
+        items.sizes = sizes.slice(Math.floor(Math.random()*4));
+        return items
+      });
+       localStorage.setItem('newProducts', JSON.stringify(newProducts));
+    }
+  });
+}else{
+    window.location.href = "../login.html"
+}
+
+
+
+
+// rendering mans products
+let MenClothingDivs = document.getElementById('mens_cards');
+
+function renderManProducts(products){
+   let manFilterArr =   products.filter((items)=>items.category === "men's clothing");
+   MenClothingDivs.innerHTML = '';
+         manFilterArr.forEach(()=>{
+
+         })
+         manFilterArr.forEach((item)=>{
+    MenClothingDivs.innerHTML +=`
+            <div class="item">
+              <img src="${item.image}" alt="Item" />
+              <div class="info">
+                <div class="row">
+                  <div class="price">$${item.price}</div>
+                  <div class="sized">${item.sizes[0] || 'S'}, ${item.sizes[1] || "M" }, ${item.sizes[2] || "L"}</div>
+                </div>
+                <div class="colors">
+                  Colors:
+                  <div class="row">
+                    <div class="circle" style="background-color: ${item.colors[0]}"></div>
+                    <div class="circle" style="background-color: ${item.colors[1] || "pink"} "></div>
+                    <div class="circle" style="background-color: ${item.colors[2] || "yellow"}"></div>
+                  </div>
+                </div>
+                <div class="row">Rating: ${item.rating.rate}</div>
+              </div>
+              <button id="${item.id}">Add to Cart</button>
+            </div>
+         `
+    })
+}
+
+
+
+// rendering womens products
+let womensDivs = document.getElementById('womens_cards')
+
+function renderWomensProducts(products){
+     let womensFilterArr = products.filter((items)=> items.category === "women's clothing");
+    //  console.log(womensFilterArr);
+
+     womensDivs.innerHTML = '';
+
+     womensFilterArr.forEach((item)=>{
+      womensDivs.innerHTML +=`
+              <div class="item">
+                <img src="${item.image}" alt="Item" />
+                <div class="info">
+                  <div class="row">
+                    <div class="price">$${item.price}</div>
+                    <div class="sized">${item.sizes[0] || 'S'}, ${item.sizes[1] || "M" }, ${item.sizes[2] || "L"}</div>
+                  </div>
+                  <div class="colors">
+                    Colors:
+                    <div class="row">
+                      <div class="circle" style="background-color: ${item.colors[0]}"></div>
+                      <div class="circle" style="background-color: ${item.colors[1] || "pink"} "></div>
+                      <div class="circle" style="background-color: ${item.colors[2] || "yellow"}"></div>
+                    </div>
+                  </div>
+                  <div class="row">Rating: ${item.rating.rate}</div>
+                </div>
+                <button id="${item.id}">Add to Cart</button>
+              </div>
+           `
+      })
+
+}
+
+
+
+
+// rendering jewelery products
+let jeweleryDivs = document.getElementById('jewelery_cards')
+
+function renderJeweleryProducts(products){
+     let jeweleryProducts = products.filter((items)=> items.category === "jewelery");
+    //  console.log(womensFilterArr);
+
+     jeweleryDivs.innerHTML = '';
+
+     jeweleryProducts.forEach((item)=>{
+      jeweleryDivs.innerHTML +=`
+              <div class="item">
+                <img src="${item.image}" style="height: 30px;"  alt="Item" />
+                <div class="info">
+                  <div class="row">
+                    <div class="price">$${item.price}</div>
+                    <div class="sized">${item.sizes[0] || 'S'}, ${item.sizes[1] || "M" }, ${item.sizes[2] || "L"}</div>
+                  </div>
+                  <div class="colors">
+                    Colors:
+                    <div class="row">
+                      <div class="circle" style="background-color: ${item.colors[0]}"></div>
+                      <div class="circle" style="background-color: ${item.colors[1] || "pink"} "></div>
+                      <div class="circle" style="background-color: ${item.colors[2] || "yellow"}"></div>
+                    </div>
+                  </div>
+                  <div class="row">Rating: ${item.rating.rate}</div>
+                </div>
+                <button id="${item.id}">Add to Cart</button>
+              </div>
+           `
+      })
+
+}
+
+
+
+// rendering jewelery products
+let electronicsDivs = document.getElementById('electronics_cards')
+
+function renderElectronicsProducts(products){
+     let electronicsProducts = products.filter((items)=> items.category === "electronics");
+    //  console.log(womensFilterArr);
+
+    electronicsDivs.innerHTML = '';
+
+    electronicsProducts.forEach((item)=>{
+      electronicsDivs.innerHTML +=`
+              <div class="item">
+                <img src="${item.image}" alt="Item" />
+                <div class="info">
+                  <div class="row">
+                    <div class="price">$${item.price}</div>
+                    <div class="sized">${item.sizes[0] || 'S'}, ${item.sizes[1] || "M" }, ${item.sizes[2] || "L"}</div>
+                  </div>
+                  <div class="colors">
+                    Colors:
+                    <div class="row">
+                      <div class="circle" style="background-color: ${item.colors[0]}"></div>
+                      <div class="circle" style="background-color: ${item.colors[1] || "pink"} "></div>
+                      <div class="circle" style="background-color: ${item.colors[2] || "yellow"}"></div>
+                    </div>
+                  </div>
+                  <div class="row">Rating: ${item.rating.rate}</div>
+                </div>
+                <button id="${item.id}">Add to Cart</button>
+              </div>
+           `
+      })
+
+}
+
+
+
+
